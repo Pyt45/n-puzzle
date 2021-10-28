6d0d5f736994235802638090ef08b7b0875e2bb5
pyt45 <ayoub1234aqlzim@gmail.com>
2021-10-28 17:59:14 +0100

    CONVERT THE PROBLEM INTO GRAPHS (matrix or red black tree)

--- src/Matrix.hpp
+++ src/Matrix.hpp
@@ -4,7 +4,30 @@
 #include <map>
 #include <vector>
 
+#define MAX_H 64
+#define MAX_W 64
+#define MIN_H 3
+#define MIN_W 3
+
 class Matrix {
     private:
+        int **arr;
+        int H, W;
     public:
+        Matrix();
+        ~Matrix();
+
+        int& operator[](const int& i);
 };
+
+/*
+
+3 2 6
+1 4 0
+8 7 5
+
+6 -> 0
+5 -> 0
+4 -> 0
+
+*/
